3bfb368c7f4694c58f1cb1b9830c8ada6c07e9e2
Alan <alanaw1@berkeley.edu>
2023-02-08 13:10:13 -0800

    fix PCA script to point to correct directories

--- scripts/get_gw_PCs1-5.R
+++ scripts/get_gw_PCs1-5.R
@@ -1,21 +1,17 @@
-###################
-# genome-wide PCA #
-###################
+## Script for performing PCA to obtain top five principal components
 
-## Option 1
-geno_dir <- "/Users/alanaw/Documents/research/pgs/082821/genotypes/"
-gw_data <- matrix(nrow=445)
-for (chr in 1:22) {
-  genotype_data <- read.csv(paste0(geno_dir, "chr", chr, "_allele_dosage.csv"))[,-1]
-  gw_data <- cbind(gw_data, genotype_data)
-}
-
-## Option 2
+# Load rds object (obtained from bigsnpr)
+# Dataset source: https://rdrr.io/github/privefl/mypack/src/data-raw/1000G-phase3-common-norel.R 
+# Follow script above to obtain the rds/bk/bgen/bed files required
 library(bigsnpr)
 test <- snp_attach("~/Documents/research/pgs/082821/1000G_phase3_common_norel/1000G_phase3_common_norel.rds")
-chr<-3
-genotype_data <- read.csv(paste0(geno_dir, "chr", chr, "_allele_dosage.csv"))
+
+# Grab individual IDs for performing PCA
+chr <- 17 # can be any autosome
+genotype_data <- read.csv(paste0("../data/geno_pheno/genotypes/chr", chr, "_allele_dosage.csv")) # need to unzip the file
 geuvadis.ids <- genotype_data[,1]
+
+# PCA 
 obj.svd <- snp_autoSVD(test$genotypes, 
                        infos.chr = test$map$chromosome,
                        ind.row = which(test$fam$sample.ID %in% geuvadis.ids), 
@@ -26,4 +22,5 @@ norm.pcs.matrix <- apply(pcs.matrix,2,scale) %>% as.data.frame()
 colnames(norm.pcs.matrix) <- paste0("PC",1:5)
 norm.pcs.matrix$ID <- test$fam$sample.ID[which(test$fam$sample.ID %in% geuvadis.ids)]
 
-save(norm.pcs.matrix, file = "gw_PC1-5.RData")
+# Save data
+save(norm.pcs.matrix, file = "../data/geno_pheno/metadata/PCA/gw_PC1-5.RData")
